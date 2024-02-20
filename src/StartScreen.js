6b88ef81654b5ea0bee4437d76bf8891d8c3199d
Rahimah Ansari <rahimeansari7@gmail.com>
2024-02-20 07:43:27 +0430

    Starting a new question

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div>
       <h2>Welcome To The React Quiz.</h2>
       <h3>{numQuestions} questions to test your React master</h3>
-      <button className='btn btn-ui'>Let's Start</button>
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: 'start' })}>
+        Let's Start
+      </button>
     </div>
   );
 }
